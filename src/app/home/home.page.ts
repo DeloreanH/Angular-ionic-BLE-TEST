853267ba97e4ced7f33e657f964020d5d2871a20
Dennys González <dgonzalez@virtwoo.com>
2019-09-18 17:01:37 -0400

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component , OnDestroy } from '@angular/core';
+import { ToastController, NavController, NavParams } from '@ionic/angular';
+import { BLE } from '@ionic-native/ble/ngx';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnDestroy {
+  public devices: any[] = [];
+  scanSubscription: Subscription;
+  connectSubscription: Subscription;
 
-  constructor() {}
+  constructor(
+    private toastCtrl: ToastController,
+    private ble: BLE,
+  ) {}
+
+  startScan() {
+    this.scanSubscription = this.ble.startScanWithOptions([], { reportDuplicates: false })
+      .subscribe( device => {
+        console.log('Found a BLE device', device);
+        this.devices.push(device);
+      });
+
+    setTimeout(() => {
+        this.ble.stopScan()
+          .then(() => console.log('Scan stopped successfully'))
+          .catch(err => console.log('Scan stopped error', err));
+      }, 3000);
+  }
+
+  connectToDevice(id: string) {
+    console.log('Connect to device with id:', id);
+    this.connectSubscription = this.ble.connect(id)
+      .subscribe((data) => {
+        console.log('Connect data', data);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.connectSubscription.unsubscribe();
+    this.scanSubscription.unsubscribe();
+  }
 
 }
